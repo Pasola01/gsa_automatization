32be09f93b04c531ff3e0245491a095f516573c5
Pasola01 <pasolchik@gmail.com>
2022-12-18 01:11:08 +0200

    add few buttons

--- gsa.py
+++ gsa.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import random
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(executable_path='./chromedriver')
 driver.set_window_size(1108, 719)
@@ -20,7 +22,36 @@ time.sleep(5)
 
 add_nominee_button = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/button').click()
 
+noninee_name_input = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div[2]/form/div[1]/div/label/input')
+noninee_name_input.send_keys('user1')
+time.sleep(5)
+
+emil_input_field = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div[2]/form/div[2]/div/label/input')
+emil_input_field.send_keys('hello1@mailinator.com')
+
+website_input_field = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div[2]/form/div[3]/div/label/input')
+website_input_field.send_keys('com.com')
+
+competition_selector = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div[2]/form/div[4]/div/div/div/div').click()
+
+select_competition = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div[2]/form/div[4]/div/div/div/div[2]').click()
+
+category_selector = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div[2]/form/div[4]/div[2]/div/div/div').click()
+time.sleep(3)
+
+choosen_category = driver.find_element(By.ID, 'rw_1_listbox_active_option')
+choosen_category.click()
+
+nominees_country_selector = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div[2]/form/div[4]/div[3]/div/div/div').click()
+
+selected_country = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div[2]/form/div[4]/div[3]/div/div/div[2]/div').click()
+time.sleep(5)
+
+invite_button = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div[2]/form/div[5]/button').click()
 
 
 breakpoint()
+
+
+
 
