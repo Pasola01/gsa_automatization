34221e07a1d61b1ffa46a06d3d57ce2921d1d362
Pasola01 <pasolchik@gmail.com>
2022-12-17 20:40:48 +0200

    add nominee button

--- gsa.py
+++ gsa.py
@@ -16,9 +16,11 @@ login_password_input.send_keys('Password132')
 switch_admin_button = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/ul/li[2]').click()
 
 login_button = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/form/button').click()
+time.sleep(5)
 
-add_nominee_button = driver.find_element(By.XPATH, '')
-add_nominee_button.click()
+add_nominee_button = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/button').click()
+
+
 
 breakpoint()
 
