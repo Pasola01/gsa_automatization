8d3d168ba3a5012b82539f625ea3cf7802c3d1e8
Pasola01 <pasolchik@gmail.com>
2022-12-19 12:03:42 +0200

    create a folder

--- ambassador/add_new_nominee.py
+++ ambassador/add_new_nominee.py
@@ -4,7 +4,7 @@ import time
 import random
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome(executable_path='./chromedriver')
+driver = webdriver.Chrome(executable_path='../chromedriver')
 driver.set_window_size(1108, 719)
 
 driver.get("http://gsa-admin-dev.s3-website.eu-central-1.amazonaws.com/")
